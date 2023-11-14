536094092cb3b8a333b9319575ef5bc4361b4dc4
Programmer <Cah9l@live.ru>
2023-11-14 14:11:06 +0300

    WORK COMMIT

--- src/models.py
+++ src/models.py
@@ -1,9 +1,38 @@
-from sqlalchemy import Table, MetaData, Integer, Column, String
+import enum
+from typing import Annotated
+
+from sqlalchemy import ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
+import datetime
+import enum
+
+
+int_pk = Annotated[int, mapped_column(primary_key=True)]
 
 
 class WorkersOrm(Base):
     __tablename__ = "workers"
-    id: Mapped[int] = mapped_column(primary_key=True)
+
+    id: Mapped[int_pk]
     username: Mapped[str]
+
+
+class Workload(enum.Enum):
+    part_time = "part_time"
+    full_time = "full_time"
+
+
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[int_pk]
+    title: Mapped[str]
+    compensations: Mapped[int | None]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
+    create_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('uts', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('uts', now())"),
+                                                          onupdate=datetime.datetime.utcnow)
+
+
